404ccda1870a16fecefbf9960d6d57a61d62e017
R4Y <fredrobinson697@hotmail.com>
2024-01-28 16:15:35 +0100

    done changes to cloud script

--- create-app.sh
+++ create-app.sh
@@ -1,9 +1,16 @@
 oc delete imagestream,buildconfig,deploymentconfig,service {product,user,client,api-gateway,email}
 
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --name api-gateway --context-dir api-gateway
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --name product --context-dir services/product
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --name client --context-dir services/client
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --name user --context-dir services/user
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --name email --context-dir services/email
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL=$(oc get secret postgresql -o jsonpath='{.data.database-url}' | base64 -d) --name api-gateway --context-dir api-gateway
+
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL=$(oc get secret postgresql -o jsonpath='{.data.database-url}' | base64 -d) --name product --context-dir services/product
+oc set env --from secret/postgresql dc/product
+
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL=$(oc get secret postgresql -o jsonpath='{.data.database-url}' | base64 -d) --name client --context-dir services/client
+oc set env --from secret/postgresql dc/client
+
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL=$(oc get secret postgresql -o jsonpath='{.data.database-url}' | base64 -d) --name user --context-dir services/user
+oc set env --from secret/postgresql dc/user
+
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL=$(oc get secret postgresql -o jsonpath='{.data.database-url}' | base64 -d) --name email --context-dir services/email
 
 oc expose svc/api-gateway
