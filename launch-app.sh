ce4025f3e5b5b5dabd736d12fa8e471f838a5a94
R4Y <fredrobinson697@hotmail.com>
2024-01-28 20:34:54 +0100

    added cloud script, changed yaml files, added env dev

--- launch-app.sh
+++ launch-app.sh
@@ -1,4 +1,21 @@
+get_env() {
+  oc get secret postgresql -o jsonpath="{.data.$1}" | base64 -d
+}
+
+oc delete imagestream,buildconfig,deploymentconfig,service,routes {product,user,client,api-gateway,email}
+oc delete secret email-secret
+
+
 oc create -f k8s/email.yaml
-oc new-app --template email-template
+oc create -f k8s/user.yaml
+oc create -f k8s/product.yaml
+oc create -f k8s/client.yaml
+oc create -f k8s/api-gateway.yaml 
+
+
+oc new-app --template email api-gateway 
+oc new-app --template client product user --build-env=DATABASE_URL="postgresql://$(get_env database-username):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"\
+  --env=DATABASE_URL="postgresql://$(get_env database-username):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"
+oc set env --from=secret/email-secret dc/email
 oc create secret generic my-secret --from-env-file=services/email/.env
 oc set env --from=secret/email-secret dc/email
--- services/product/src/config/index.ts
+++ services/product/src/config/index.ts
@@ -4,7 +4,6 @@ dotenv.config();
 
 export default {
     PORT: process.env.PORT,
-    SECRET: process.env.SECRET,
     DATABASE_URL: process.env.DATABASE_URL || '',
     CLIENT_SERVICE_URL: process.env.CLIENT_SERVICE_URL || '',
     USER_SERVICE_URL: process.env.USER_SERVICE_URL || ''
