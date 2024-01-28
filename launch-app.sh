c0a5d0629a80471782f2b1b13cf253457672f5d7
R4Y <fredrobinson697@hotmail.com>
2024-01-28 20:52:13 +0100

    cloud script changes

--- launch-app.sh
+++ launch-app.sh
@@ -3,6 +3,7 @@ get_env() {
 }
 
 oc delete imagestream,buildconfig,deploymentconfig,service,routes {product,user,client,api-gateway,email}
+oc delete template {product,user,client,api-gateway,email}-template
 oc delete secret email-secret
 
 
@@ -12,10 +13,10 @@ oc create -f k8s/product.yaml
 oc create -f k8s/client.yaml
 oc create -f k8s/api-gateway.yaml 
 
+oc create secret generic email-secret --from-env-file=services/email/.env
 
-oc new-app --template email api-gateway 
-oc new-app --template client product user --build-env=DATABASE_URL="postgresql://$(get_env database-username):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"\
+oc new-app --template client-template product-template user-template --build-env=DATABASE_URL="postgresql://$(get_env database-username):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"\
   --env=DATABASE_URL="postgresql://$(get_env database-username):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"
+oc new-app --template api-gateway-template email-template 
 oc set env --from=secret/email-secret dc/email
-oc create secret generic my-secret --from-env-file=services/email/.env
 oc set env --from=secret/email-secret dc/email
