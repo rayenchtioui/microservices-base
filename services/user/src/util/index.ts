c8ce03383b117e08843492c47d4890a7948a3823
R4Y <fredrobinson697@hotmail.com>
2024-01-29 18:42:24 +0100

    added unit tests

--- services/user/src/util/index.ts
+++ services/user/src/util/index.ts
@@ -14,3 +14,5 @@ export const PublishClientEvent = async (payload:{event:string, data:any}) => {
         console.log(err.toString());
     });
 }
+
+export const equation = (a:number, b:number) => a + b*b
