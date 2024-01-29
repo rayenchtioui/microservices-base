a88445acfe0a395062156968cde70a84d51eda51
R4Y <fredrobinson697@hotmail.com>
2024-01-29 15:54:17 +0100

    configured dockerfiles and script

--- create-app.sh
+++ create-app.sh
@@ -6,15 +6,15 @@ oc delete imagestream,buildconfig,deploymentconfig,service,routes {product,user,
 oc delete template {product,user,client,api-gateway,email}-template
 oc delete secret email-secret
 
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL="postgresql://$(get_env database-user):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL="postgresql://$(get_env database-user):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public" --name api-gateway --context-dir api-gateway
 
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL="postgresql://$(get_env database-user):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL="postgresql://$(get_env database-user):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public" --name product --context-dir services/product
 oc set env --from secret/postgresql dc/product
 
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL="postgresql://$(get_env database-user):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL="postgresql://$(get_env database-user):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public" --name client --context-dir services/client
 oc set env --from secret/postgresql dc/client
 
-oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL="postgresql://$(get_env database-user):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public"
+oc new-app https://github.com/rayenchtioui/microservices-base  --as-deployment-config=true --build-env=DATABASE_URL="postgresql://$(get_env database-user):$(get_env database-password)@postgresql:5432/$(get_env database-name)?schema=public" --name user --context-dir services/user
 oc set env --from secret/postgresql dc/user
 
 oc create -f k8s/email.yaml
