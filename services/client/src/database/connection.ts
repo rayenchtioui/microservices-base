b6a4cbe51dc3ed592ecae42450e72e3cf47b56b9
R4Y <fredrobinson697@hotmail.com>
2024-01-29 14:14:54 +0100

    removed env variables

--- services/client/src/database/connection.ts
+++ services/client/src/database/connection.ts
@@ -2,19 +2,9 @@ import config from '../config';
 
 const connect = async () => {
     console.log('Connecting to DB');
-    const dbUser = config.DATABASE_USERNAME;
-    const dbPass = config.DATABASE_PASSWORD;
-    const dbHost = config.DATABASE_HOST;
-    if(!dbUser || dbUser.length < 1) {
+    const dbUrl = config.DATABASE_URL;
+    if(!dbUrl || dbUrl.length < 1) {
         console.log('No database Username found');
-        return;
-    }
-    if(!dbPass || dbPass.length < 1) {
-        console.log('No database Password found');
-        return;
-    }
-    if(!dbHost || dbHost.length < 1) {
-        console.log('No database Host found');
         return;
     }
     // Handle database connection here
