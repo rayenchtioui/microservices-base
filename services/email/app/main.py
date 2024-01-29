99f215cd2a26ace30d740a88405a96d5a0716a75
rayenchtioui <chtiouirayyen@gmail.com>
2024-01-30 00:57:56 +0100

    fix

--- services/email/app/main.py
+++ services/email/app/main.py
@@ -3,14 +3,25 @@ from fastapi import BackgroundTasks, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app import routers
 from app.routers.logs import monitor_pods
-
+import asyncio
+import threading
 
 app = FastAPI()
+async def periodic_monitoring():
+    while True:
+        await monitor_pods()
+        await asyncio.sleep(100)
+
+def start_background_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
 @app.on_event("startup")
-async def start_monitoring():
-    print("here")
-    BackgroundTasks.add_task(periodic_monitoring)
-    return {"message": "Monitoring started in the background"}
+def startup_event():
+    print("Starting monitoring in the background")
+    loop = asyncio.new_event_loop()
+    threading.Thread(target=start_background_loop, args=(loop,), daemon=True).start()
+    asyncio.run_coroutine_threadsafe(periodic_monitoring(), loop)
 origins = ["*"]
 
 app.add_middleware(
@@ -26,9 +37,3 @@ app.include_router(routers.logs.router)
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-async def periodic_monitoring():
-    while True:
-        await monitor_pods()
-        await asyncio.sleep(100) 
-
