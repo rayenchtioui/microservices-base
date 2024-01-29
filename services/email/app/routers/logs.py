261380d4fb90a9752ef1dc7f006ffe77b2b49acb
rayenchtioui <chtiouirayyen@gmail.com>
2024-01-29 11:44:05 +0100

    kubernetes roles authorization

--- services/email/app/routers/logs.py
+++ services/email/app/routers/logs.py
@@ -3,6 +3,7 @@ from .emailUtil import send_email
 from kubernetes import client, config, watch
 from openshift.dynamic import DynamicClient
 import requests
+import httpx
 
 router = APIRouter(
     prefix="/logs",
@@ -21,19 +22,22 @@ async def send_logs(entry: str, email: str):
 
 fastapi_endpoint = "http://email-test-email.apps.na46a.prod.ole.redhat.com/sendLogs"
 
-def send_failure_notification(email, message):
+async def send_failure_notification(email, message):
     payload = {'email': email, 'entry': message}
-    response = requests.post(fastapi_endpoint, json=payload)
-    print(response.text)
+    async with httpx.AsyncClient() as client:
+        response = await client.post(fastapi_endpoint, json=payload)
+        print(response)
 
 async def monitor_pods():
+    print("monitoring pods")
+    await send_failure_notification("chtiouirayyen@gmail.com", "Pod  has failed.")
     config.load_incluster_config()
     k8s_client = client.ApiClient()
     dyn_client = DynamicClient(k8s_client)
     v1_pods = dyn_client.resources.get(api_version='v1', kind='Pod')
     w = watch.Watch()
 
-    for event in w.stream(v1_pods.get, namespace='default'):
+    for event in w.stream(v1_pods.get, namespace='test-email'):
         pod = event['object']
         if pod.status.phase == "Failed":
             await send_failure_notification("chtiouirayyen@gmail.com", f"Pod {pod.metadata.name} has failed.")
