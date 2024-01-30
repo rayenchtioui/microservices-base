8e2c4dc57ebaba45ee30d5cf47311e581953be99
rayenchtioui <chtiouirayyen@gmail.com>
2024-01-30 01:53:40 +0100

    fix

--- services/email/app/main.py
+++ services/email/app/main.py
@@ -1,30 +1,39 @@
-from fastapi import APIRouter
-from .emailUtil import send_email
-from kubernetes import client, config, watch
-from openshift.dynamic import DynamicClient
-import requests
-import httpx
+import asyncio
+from fastapi import BackgroundTasks, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app import routers
+from app.routers.logs import monitor_pods
+import asyncio
+import threading
 
-router = APIRouter(
-    prefix="/logs",
-    tags=['Logs']
+app = FastAPI()
+async def periodic_monitoring():
+    while True:
+        await monitor_pods()
+        await asyncio.sleep(100)
+
+def start_background_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+@app.on_event("startup")
+def startup_event():
+    print("Starting monitoring in the background")
+    loop = asyncio.new_event_loop()
+    threading.Thread(target=start_background_loop, args=(loop,), daemon=True).start()
+    asyncio.run_coroutine_threadsafe(periodic_monitoring(), loop)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-async def send_mail_logs(email: str,pod_name: str):
-    subject = "Logs"
-    recipients = [email]
-    await send_email(subject, recipients, email, pod_name)
+app.include_router(routers.logs.router)
 
-
-async def monitor_pods():
-    print("monitoring pods")
-    config.load_incluster_config()
-    k8s_client = client.ApiClient()
-    dyn_client = DynamicClient(k8s_client)
-    v1_pods = dyn_client.resources.get(api_version='v1', kind='Pod')
-    w = watch.Watch()
-    print("v1_pods.get:", v1_pods.get)
-    for event in w.stream(v1_pods.get, namespace='test'):
-        pod = event['object']
-        if pod.status.phase == "Failed":
-            await send_mail_logs("chtiouirayyen@gmail.com", f"Pod {pod.metadata.name} has failed.")
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
